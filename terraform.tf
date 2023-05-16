7db85651554617c427535d876199f2ffa60d240e
Yosiah de Koeyer <dev@yosiahdekoeyer.dev>
2023-05-16 22:47:55 +1000

    Add tf cloud and GA workflows

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,11 @@
 terraform {
+  cloud {
+    organization = "Y0sh1-personal"
+
+    workspaces {
+      name = "github-provisioning"
+    }
+  }
   required_providers {
     github = {
       source  = "integrations/github"
